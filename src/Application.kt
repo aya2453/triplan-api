2fa7631ad7cbab016edfbef58ef0a506e0cbf545
中川　彩 <aya_nakagawa@r.recruit.co.jp>
2018-11-23 01:51:31 +0900

    Ktorの導入

---
+++ src/Application.kt
@@ -0,0 +1,57 @@
+package `fun`.triplan
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.jackson.jackson
+import java.util.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!")
+        }
+
+        get("/snippets") {
+            call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+        }
+    }
+}
+
+
+data class Snippet(val text: String)
+
+val snippets = Collections.synchronizedList(
+    mutableListOf(
+        Snippet("hello"),
+        Snippet("world")
+    )
+)
